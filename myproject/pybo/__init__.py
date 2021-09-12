326e65c256f468c01e1dd5adbe5fe15f38b834f9
Jung <rlckrldb@gmail.com>
2021-09-12 22:23:59 +0900

    2-06 답변 등록 기능 만들기

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -17,8 +17,10 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views, question_views
+    # from .views import main_views, question_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)# 추가
 
     return app
